c42bc691df894951afec1d94f9b585070cf4ef65
assane savadogo <assane.savadogo81@gmail.com>
2019-06-21 18:22:41 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+ new_hash = {}
+ data.each do |key, value|
+   value.each do |item, names|
+     names.each do |name|
+       new_hash[name] ||= {}
+       new_hash[name][key] ||= []
+       new_hash[name][key] << item.to_s 
+     end
+   end
+ end
+ new_hash
+ end
+
+
+  
+
+ #Rather than sorting by attributes, your task is to create a hash where pigeon names are the keys, with each name pointing to a hash of their attributes.
+ 
+ #pigeon_list = {
+ # "Theo" => {
+  #  :color => ["purple", "grey"],
+  
